c5ce5af20083db72d917f463865e06e3cf8bcd40
cleberdasilva <cleber.supertux@gmail.com>
2026-02-07 14:00:17 -0300

    Project structure and base files

---
+++ src/test/java/com/drool/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drool.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
